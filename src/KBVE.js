dc7ddb4dd3772a5e20c48040c31d48d008826752
h0lybyte <5599058+h0lybyte@users.noreply.github.com>
2023-09-07 22:16:00 -0400

    Deployment Update

--- src/KBVE.js
+++ src/KBVE.js
@@ -1,12 +1,60 @@
 import React from "react";
-import * as Locker from './Locker';
+import * as Locker from "./Locker";
+import { useStore } from "@nanostores/react";
+import { client } from "@gradio/client";
 
 const KBVE = ({ ve }) => {
-  const category = ve.getAttribute("category") ? ve.getAttribute("category") : "General";
-  const img = ve.getAttribute("img") ? ve.getAttribute("img") : "https://source.unsplash.com/random/480x360/?4"
-  const title = ve.getAttribute("title") ? ve.getAttribute("title") : "Title";
-  const link = ve.getAttribute("link") ? ve.getAttribute("link") : "/#";
+  const $title = useStore(Locker.title$);
+  const $category = useStore(Locker.category$);
+  const $img = useStore(Locker.img$);
+  const $link = useStore(Locker.link$);
+  const $description = useStore(Locker.description$);
+  const $n8n = useStore(Locker.n8n$);
+  const $load = useStore(Locker.load$);
 
+  React.useEffect(() => {
+    if (ve.getAttribute("title"))
+      Locker.tasker(Locker.title$, ve.getAttribute("title"));
+    if (ve.getAttribute("category"))
+      Locker.tasker(Locker.category$, ve.getAttribute("category"));
+    if (ve.getAttribute("img"))
+      Locker.tasker(Locker.img$, ve.getAttribute("img"));
+    if (ve.getAttribute("link"))
+      Locker.tasker(Locker.link$, ve.getAttribute("link"));
+    if (ve.getAttribute("description"))
+      Locker.tasker(Locker.description$, ve.getAttribute("description"));
+    if (ve.getAttribute("n8n"))
+      Locker.tasker(Locker.n8n$, ve.getAttribute("n8n"));
+    Locker.tasker(Locker.load$, false);
+  }, []);
+
+  const seamlessM4T = async (lang) => {
+    Locker.tasker(Locker.load$, true);
+    console.log(`Fetching Data for ${lang}`);
+    makeApiCall();
+  };
+
+  const fetchAudio = async () => {
+    const response = await fetch(
+      "https://github.com/gradio-app/gradio/raw/main/test/test_files/audio_sample.wav"
+    );
+    return await response.blob();
+  };
+
+  const makeApiCall = async () => {
+    const exampleAudio = await fetchAudio();
+    const app = await client("https://gratitud3-seamless-m4t.hf.space/");
+    const result = await app.predict("/run", [
+      "S2ST (Speech to Speech translation)",
+      "file",
+      exampleAudio,
+      exampleAudio,
+      "Howdy!",
+      "Afrikaans",
+      "Bengali",
+    ]);
+    console.log(result.data);
+  };
 
   return (
     <>
@@ -15,9 +63,9 @@ const KBVE = ({ ve }) => {
           <div className="flex items-center">
             <a
               rel="noopener noreferrer"
-              href={link}
+              href={$link}
               className="mb-0 capitalize dark:text-gray-100">
-              {category}
+              {$category}
             </a>
           </div>
           <a rel="noopener noreferrer" href="#">
@@ -27,25 +75,48 @@ const KBVE = ({ ve }) => {
         <div className="space-y-4">
           <div className="space-y-2">
             <img
-              src={img}
+              src={$img}
               alt=""
               className="block object-cover object-center w-full rounded-md h-72 dark:bg-gray-500"
             />
-            <div className="flex items-center text-xs">
-              <span>6 min ago</span>
+            <div className="flex items-center text-xs flex divide-x-2 rounded dark:text-gray-100 divide-gray-700">
+              <button
+                type="button"
+                className="px-3 py-1"
+                onClick={() => seamlessM4T("en")}>
+                EN
+              </button>
+              <button
+                type="button"
+                className="px-3 py-1"
+                onClick={() => seamlessM4T("de")}>
+                DE
+              </button>
+              <button
+                type="button"
+                className="px-3 py-1"
+                onClick={() => seamlessM4T("fr")}>
+                FR
+              </button>
             </div>
           </div>
+
           <div className="space-y-2">
-            <a rel="noopener noreferrer" href="#" className="block">
-              <h3 className="text-xl font-semibold dark:text-orange-400">
-                Facere ipsa nulla corrupti praesentium pariatur architecto
-              </h3>
-            </a>
-            <p className="leadi dark:text-gray-400">
-              Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-              Repellat, excepturi. Lorem ipsum dolor sit amet consectetur,
-              adipisicing elit. Repellat, excepturi.
-            </p>
+            {$load && (
+              <div className="flex items-center justify-center space-x-2">
+                <div className="w-4 h-4 rounded-full animate-pulse bg-orange-400"></div>
+                <div className="w-4 h-4 rounded-full animate-pulse bg-orange-400"></div>
+                <div className="w-4 h-4 rounded-full animate-pulse bg-orange-400"></div>
+              </div>
+            )}{" "}
+            <>
+              <a rel="noopener noreferrer" href="#" className="block">
+                <h3 className="text-xl font-semibold dark:text-orange-400">
+                  {$title}
+                </h3>
+              </a>
+              <p className="leadi dark:text-gray-400">{$description}</p>
+            </>
           </div>
         </div>
       </div>
--- src/Locker.js
+++ src/Locker.js
@@ -1,18 +1,34 @@
-import { atom, task } from "nanostores";
-import {
-  Account,
-  AppwriteException,
-  Client,
-  Functions,
-  Databases,
-  Storage,
-  ID,
-  Models,
-  Query,
-} from "appwrite";
+import { atom, task, keepMount  } from "nanostores";
+// import {
+//   Account,
+//   AppwriteException,
+//   Client,
+//   Functions,
+//   Databases,
+//   Storage,
+//   ID,
+//   Models,
+//   Query,
+// } from "appwrite";
 
-export const appwriteClient = new Client()
-  .setEndpoint("https://ap.kbve.com/v1")
-  .setProject("6436a6dc9a6b48db802f");
+// export const appwriteClient = new Client()
+//   .setEndpoint("https://ap.kbve.com/v1")
+//   .setProject("6436a6dc9a6b48db802f");
 
-  export const api$ = atom(false);
+// export const api$ = atom(false);
+
+export const title$ = atom('Title');
+export const category$ = atom('General');
+export const img$ = atom('https://source.unsplash.com/random/480x360/?4');
+export const link$ = atom('/#');
+export const description$ = atom('Description');
+export const n8n$ = atom('');
+export const load$ = atom(true);
+
+export const tasker = async (__key, __data) => {
+	task(async () => {
+		console.log(`Storing ${__data} into atom!`);
+		__key.set(__data);
+		keepMount(__key);
+	});
+};
